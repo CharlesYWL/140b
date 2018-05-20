e97877aa7d89d56c6079c93ec4360572bfe297c2
Weili Yin <xlyin@ucdavis.edu>
2018-05-20 05:52:29 -0700

    stuck on figuring out space for pawns

--- CTF/ctf.hs
+++ CTF/ctf.hs
@@ -1,11 +1,12 @@
 import Data.Char
-
+import Data.Ix
 --capture::[String]->Char->Int->String
 --capture l c n
 
-findRow::String->Float
-findRow l = sqrt (fromIntegral  (length l))
-
+findRow::String->Int
+findRow l = toInt (sqrt (fromIntegral  (length l)))
+toInt :: Float -> Int
+toInt x = round x
 --this function will return the evaluation
 --evalueBoard::String->Char->Int
 --evalueBoard board side
@@ -14,7 +15,7 @@ findRow l = sqrt (fromIntegral  (length l))
 evBw::String->Int
 evBw board
   |or[not (elem 'B' board), (countNum 'b' board) ==0,not (canImove board 'b')] = 1000 --means white wins!
-  |or[not (elem 'W' board), (countNum 'w' board) ==0] = -1000 --means white loses.
+  |or[not (elem 'W' board), (countNum 'w' board) ==0,not (canImove board 'w')] = -1000 --means white loses.
   |otherwise = (countNum 'w' board) - (countNum 'b' board)
 
 --countNum will count how many elem of target in list
@@ -28,9 +29,13 @@ canImove board s = canImove_h board s 0
 canImove_h::String->Char->Int->Bool
 canImove_h board s pos
   |pos >= length board = False--test till last pos, means all fail
-  |and[(board !! pos)==s,or[] ] = True--test pawn, inside or[],freespace aviliable
-  |and[(board !! pos)==(toUpper s),or[]] = True--test flag
-  |otherwise = canImove_h board s (pos+1) --this cannot move, next
+  |and[(board !! pos)==s,or[]] = True--test pawn, inside or[],freespace aviliable
+  |and[(board !! pos)==(toUpper s),or (map (isfree board) (getFPos (findRow board) pos))] =True--test flag
+  |otherwise = canImove_h board s (pos+1) --this cannot move/or not yours, next
 --free func test if a space is empty
 isfree::String->Int->Bool
 isfree board pos = (board !! pos)=='-'
+getFPos::Int->Int->[Int] --get aviliable slots for Flag
+getFPos row pos = filter (inRange(0,row^2-1)) [pos-row,pos-1,pos+1,pos+row]
+getPPos::[String]->Int->Int->[Int]--get aviliable slots for pawns
+getPPos board row pos = filter (inRange(0,row^2-1)) [pos-row,pos-1,pos+1,pos+row] ++
