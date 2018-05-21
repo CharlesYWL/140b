164416bbc9cf54020562501507a13c0ae973b432
Weili Yin <xlyin@ucdavis.edu>
2018-05-20 18:40:21 -0700

    evluation parts done (algo is pretty simple, only campare how many pawn)

--- CTF/ctf.hs
+++ CTF/ctf.hs
@@ -10,6 +10,10 @@ getPosCol::Int->Int->Int
 getPosCol row pos = mod pos row
 getPosRow::Int->Int->Int
 getPosRow row pos = quot pos row
+isSameRow::Int->Int->Int->Bool
+isSameRow row pos pos2 = getPosRow row pos==getPosRow row pos2
+isSameCol::Int->Int->Int->Bool
+isSameCol row pos pos2 = getPosCol row pos==getPosCol row pos2
 toInt :: Float -> Int
 toInt x = round x
 opp::Char->Char --find oppenents
@@ -30,6 +34,11 @@ evBw board
   |or[not (elem 'B' board), (countNum 'b' board) ==0,not (canImove board 'b')] = 1000 --means white wins!
   |or[not (elem 'W' board), (countNum 'w' board) ==0,not (canImove board 'w')] = -1000 --means white loses.
   |otherwise = (countNum 'w' board) - (countNum 'b' board)
+evBb::String->Int
+evBb board
+  |or[not (elem 'W' board), (countNum 'w' board) ==0,not (canImove board 'w')] = 1000 --means white wins!
+  |or[not (elem 'B' board), (countNum 'b' board) ==0,not (canImove board 'b')] = -1000 --means white loses.
+  |otherwise = (countNum 'b' board) - (countNum 'w' board)
 
 --countNum will count how many elem of target in list
 countNum::Char->String->Int
@@ -56,17 +65,21 @@ getFPos row pos = one2one [and[isSameCol row pos (pos-row), inRange (0,row^2-1) 
                            and[isSameRow row pos (pos+1),inRange (0,row^2-1) (pos+1)],
                            and[isSameCol row pos (pos+row),inRange (0,row^2-1) (pos+row)]] [pos-row,pos-1,pos+1,pos+row]
 
-isSameRow::Int->Int->Int->Bool
-isSameRow row pos pos2 = getPosRow row pos==getPosRow row pos2
-isSameCol::Int->Int->Int->Bool
-isSameCol row pos pos2 = getPosCol row pos==getPosCol row pos2
 
 getPPos::String->Int->Int->Char->[Int]--get aviliable slots for pawns
-getPPos board row pos side = filter (inRange(0,row^2-1)) [pos-row,pos-1,pos+1,pos+row]  -- ++ (getPPos_h board row pos side)
---getPPos_h::String->Int->Int->Char->[Int]
---getPPos_h board row pos side
---  = one2one [if inRange (0,row^2-1) pos-row then (board !! pos-row)==(opp side) else False,
---             if inRange (0,row^2-1) pos-1   then (board !! pos-1)  ==(opp side) else False,
---             if inRange (0,row^2-1) pos+1   then (board !! pos+1)  ==(opp side) else False,
---             if inRange (0,row^2-1) pos+row then (board !! pos+row)==(opp side) else False
---            ]  [pos-row-row,]
+getPPos board row pos side = one2one [and[isSameCol row pos (pos-row), inRange (0,row^2-1) (pos-row), side=='b'], --white cannot move up
+                                      and[isSameRow row pos (pos-1),inRange (0,row^2-1) (pos-1)],
+                                      and[isSameRow row pos (pos+1),inRange (0,row^2-1) (pos+1)],
+                                      and[isSameCol row pos (pos+row),inRange (0,row^2-1) (pos+row),side=='w']] [pos-row,pos-1,pos+1,pos+row] --black cannot move down
+                                       ++ (getPPos_h board row pos side)
+getPPos_h::String->Int->Int->Char->[Int] -- pawns can jump over oppenents
+getPPos_h board row pos side
+  =  one2one [and[isSameCol row pos (pos-row), inRange (0,row^2-1) (pos-row),side=='b',(board !! (pos-row))==(opp side),
+                  isSameCol row pos (pos-row-row), inRange (0,row^2-1) (pos-row-row)],
+              and[isSameRow row pos (pos-1),inRange (0,row^2-1) (pos-1),(board !! (pos-1))==(opp side),
+                  isSameRow row pos (pos-2), inRange (0,row^2-1) (pos-2)],
+              and[isSameRow row pos (pos+1),inRange (0,row^2-1) (pos+1),(board !! (pos+1))==(opp side),
+                  isSameRow row pos (pos+2), inRange (0,row^2-1) (pos+2)],
+              and[isSameCol row pos (pos+row),inRange (0,row^2-1) (pos+row),side=='w',(board !! (pos+row))==(opp side),
+                  isSameCol row pos (pos+row+row), inRange (0,row^2-1) (pos+row+row)]
+             ] [pos-row-row,pos-2,pos+2,pos+row+row]
