9a2d2eedb21739ea9aba9798c7fbeffc7e41e870
CharlesYWL <xlyin@ucdavis.edu>
2018-05-29 03:28:41 -0700

    q1 done

--- HW3/funs.erl
+++ HW3/funs.erl
@@ -1,1 +1,11 @@
-test
+-module(funs).
+-export([myremoveduplicates/1,inter/2]).
+
+myremoveduplicates([]) -> [];
+myremoveduplicates([H|T]) ->
+case (lists:member(H,T)==true) of
+    true->    myremoveduplicates(T);
+    false -> [H|myremoveduplicates(T)]
+end.
+
+inter
