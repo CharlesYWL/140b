267ccbc22c720b2f583bf8b43cd0cddfb71b5e89
Weili Yin <xlyin@ucdavis.edu>
2018-06-04 12:22:36 -0700

    bank part done

--- HW4/bank.erl
+++ HW4/bank.erl
@@ -1,2 +1,19 @@
--molude(bank).
+-module(bank).
 -export([loop/0]).
+
+loop() ->
+  receive
+    {create,List,TableName} ->
+      ets:new(TableName,[named_table,public]),
+      lists:map(fun(I) -> ets:insert(TableName,I) end, List),
+      io:format("bank ~p created\n",[TableName]),
+      loop();
+    {open,Name,Money,TableName} ->
+      ets:insert(TableName,{Name,Money}),
+      io:format("new account ~p opened with ~p dollars\n",[Name,Money]),
+      loop();
+    {balance,Name,TableName} ->
+      [{Name,Doller}]=ets:lookup(TableName,Name),
+      io:format("account ~p has ~p dollars\n",[Name,Doller]),
+      loop()
+end.
