f2ef8778402ffe7430cffb835b35c96103186426
Weili Yin <xlyin@ucdavis.edu>
2018-06-04 14:34:18 -0700

    having problems on how returning value and catch that message

--- HW4/atm.erl
+++ HW4/atm.erl
@@ -1,2 +1,40 @@
 -module(atm).
 -export([loop/0]).
+
+loop()->
+  receive
+    {start,Pid,BankName,Cash,AtmName} ->
+      ets:new(AtmName,[named_table,public]),
+      ets:insert(AtmName,{cash,Cash}),
+      ets:insert(AtmName,{bankname,BankName}),
+      ets:insert(AtmName,{pid,Pid}),
+      io:format("atm ~p started with ~p dollars cash available\n",[AtmName,Cash]),
+      loop();
+    {cashsupply,AtmName} ->
+      [{cash,Cash}]= ets:lookup(AtmName,cash),
+      io:format("atm ~p has ~p dollars on hand\n",[AtmName,Cash]),
+      loop();
+    {deposit,Name,Cash,AtmName} ->
+      [{pid,Pid}] = ets:lookup(AtmName,pid),
+      [{bankname,BankName}]= ets:lookup(AtmName,bankname),
+      Pid ! {deposit,Name,Cash,BankName},
+      loop();
+%    {withdraw,Name,Cash,AtmName}->
+    {balance,Name,AtmName} ->
+      [{pid,Pid}] = ets:lookup(AtmName,pid),
+      [{bankname,BankName}]= ets:lookup(AtmName,bankname),
+      Pid ! {balance,Name,BankName},
+      loop();
+    {withdraw,Name,Cash,AtmName} ->
+      [{pid,Pid}] = ets:lookup(AtmName,pid),
+      [{bankname,BankName}]= ets:lookup(AtmName,bankname),
+      [{cash,NowCash}]= ets:lookup(AtmName,cash),
+      case (NowCash>=Cash) of
+        true ->
+          case (Pid ! {withdraw,Name,Cash,BankName}) of
+            true ->
+            false ->
+        false ->
+          io:format("sorry, insufficient cash in this atm",[]),
+          loop()      
+end.
--- HW4/bank.erl
+++ HW4/bank.erl
@@ -1,19 +1,46 @@
 -module(bank).
--export([loop/0]).
+-export([loop/0,createTable/2,insertTable/3]).
 
 loop() ->
   receive
     {create,List,TableName} ->
-      ets:new(TableName,[named_table,public]),
-      lists:map(fun(I) -> ets:insert(TableName,I) end, List),
+      createTable(List,TableName),
       io:format("bank ~p created\n",[TableName]),
       loop();
     {open,Name,Money,TableName} ->
-      ets:insert(TableName,{Name,Money}),
+      insertTable(TableName,Name,Money),
       io:format("new account ~p opened with ~p dollars\n",[Name,Money]),
       loop();
     {balance,Name,TableName} ->
       [{Name,Doller}]=ets:lookup(TableName,Name),
       io:format("account ~p has ~p dollars\n",[Name,Doller]),
-      loop()
+      loop();
+    {deposit,Name,Cash,BankName} ->
+      [{Name,OldBalance}]=ets:lookup(BankName,Name),
+%      io:format("~p had OldBalance: ~p\n",[Name,OldBalance]),
+      NewBalance=(OldBalance+Cash),
+      ets:delete(BankName,Name),
+      ets:insert(BankName,{Name,NewBalance}),
+      io:format("account ~p now has ~p dollars\n",[Name,NewBalance]),
+      loop();
+    {withdraw,Name,Cash,BankName} ->
+      [{Name,OldBalance}]=ets:lookup(BankName,Name),
+      NewBalance=(OldBalance-Cash),
+      case(NewBalance >= 0) of
+        true ->
+          ets:delete(BankName,Name),
+          ets:insert(BankName,{Name,NewBalance}),
+          io:format("~p dollars withdrawn\n",[Cash]),
+          io:format("account ~p now has ~p dollars\n",[Name,NewBalance]),
+          true;
+        false ->
+          io:format("sorry, account ~p has only ~p dollars\n",[Name,OldBalance]),
+          false;
 end.
+
+createTable(List,TableName) ->
+  ets:new(TableName,[named_table,public]),
+  lists:map(fun(I) -> ets:insert(TableName,I) end, List).
+
+insertTable(TableName,Name,Money)->
+        ets:insert(TableName,{Name,Money}).
